b29206f7ecf6d36605728192ad329eaeaeb4747c
anjalimehmi <anjalimehmi91@gmail.com>
2025-07-08 20:01:46 +0530

    Initial commit

---
+++ app/src/main/java/com/o7/fragmentsproject/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.o7.fragmentsproject
+
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+
+class MainActivity : androidx.appcompat.app.AppCompatActivity() {
+ lateinit var navController: NavController
+    lateinit var mainmenu: Unit
+    lateinit var appBarConfiguration: AppBarConfiguration
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        navController=findNavController(R.id.host)
+        appBarConfiguration=AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController,appBarConfiguration)
+    }
+}
